f37f3ef35d8cbdbe31ded3d7c36de99a0d470d98
weiyaotian <yaotianwei03@gmail.com>
2025-03-14 18:02:44 +0800

    add gravity changable fall down solution.

--- python/gravity.py
+++ python/gravity.py
@@ -1,3 +1,10 @@
+'''
+'-' represents an empty cell,
+'#' represents an obstacle,
+'F' corresponds to a cell of a connected figure.
+
+The shape of the figure is unchangable and is represented by 'F' cells.
+'''
 matrix = [
     ['F', 'F', 'F'],
     ['-', 'F', '-'],
@@ -56,4 +63,64 @@ def print_matrix(matrix):
     for row in matrix:
         print(row)
 
+
+print("unchangable solution: ")
 print_matrix(gravity(matrix=matrix))
+
+
+"""
+What if the shape of the figure is changable?
+
+"""
+matrix = [
+    ['F', 'F', 'F'],
+    ['-', 'F', '-'],
+    ['-', 'F', 'F'],
+    ['#', 'F', '-'],
+    ['F', 'F', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '#'],
+    ['-', '-', '-'],
+]
+
+
+
+expected_reuslt = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['F', '-', '-'],
+    ['#', 'F', '-'],
+    ['-', 'F', 'F'],
+    ['-', 'F', 'F'],
+    ['-', 'F', '#'],
+    ['F', 'F', '-'],
+]
+def gravity_changable(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    for col in range(cols):
+        count_f = 0
+        for row in range(rows):
+            if row == rows - 1:
+                falling_row = row 
+                while count_f > 0:
+                    matrix[falling_row][col] = 'F'
+                    falling_row -= 1
+                    count_f -= 1
+
+            elif matrix[row][col] == '#':
+                falling_row = row - 1
+                while count_f > 0:
+                    matrix[falling_row][col] = 'F'
+                    falling_row -= 1
+                    count_f -= 1
+            
+            elif matrix[row][col] == 'F':
+                matrix[row][col] = '-'
+                count_f += 1
+    
+    return matrix
+
+print("changable solution: ")
+print_matrix(gravity_changable(matrix))
